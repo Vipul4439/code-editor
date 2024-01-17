6f195308d53d2e3d8c142d5b1be78ffab0087b3e
Vipul <vipul.soni@carveniche.com>
2024-01-17 17:17:49 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const app = express();
 const cors = require("cors");
 const port = 4000;
-
+require("dotenv").config();
 app.use(express.json());
 
 app.use(cors());
@@ -12,9 +12,8 @@ app.post("/getAuthToken", async (req, res) => {
   console.log("hello there");
   try {
     const response = await axios.post("https://api.jdoodle.com/v1/auth-token", {
-      clientId: "f322e4f6b064427969418f0cb4ff525a",
-      clientSecret:
-        "4f4109eadeaa716d6fdf71b3a1eb2c56dc87ee6175fa5bc687e26fd6718a2b2d",
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
     });
     console.log("response data", response.data);
     res.json(response.data);
